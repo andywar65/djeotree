7605c7007f215f1457067b88c9c6fb46afb0a69b
andywar65 <andy.war1965@gmail.com>
2022-05-21 17:32:26 +0200

    family model

--- admin.py
+++ admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-# Register your models here.
+from .models import Family
+
+
+class FamilyAdmin(TreeAdmin):
+    form = movenodeform_factory(Family)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("user", "title", "intro"),
+            },
+        ),
+        (
+            None,
+            {
+                "fields": ("_position", "_ref_node_id"),
+            },
+        ),
+    )
+
+
+admin.site.register(Family, FamilyAdmin)
--- models.py
+++ models.py
@@ -1,3 +1,42 @@
-# from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from treebeard.mp_tree import MP_Node
 
-# Create your models here.
+User = get_user_model()
+
+
+class Family(MP_Node):
+    node_order_by = ["title"]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_family",
+        verbose_name=_("Author"),
+    )
+    parent = models.ForeignKey(
+        "self", verbose_name=_("Parent family"), null=True, on_delete=models.CASCADE
+    )
+    title = models.CharField(
+        _("Title"),
+        help_text=_("Family name"),
+        max_length=50,
+    )
+    intro = models.CharField(
+        _("Description"),
+        null=True,
+        blank=True,
+        help_text=_("Few words to describe the family"),
+        max_length=100,
+    )
+
+    class Meta:
+        verbose_name = _("Element Family")
+        verbose_name_plural = _("Element Families")
+
+    def __str__(self):
+        prefix = ""
+        for i in range(self.depth - 1):
+            prefix = prefix + "-"
+        return prefix + self.title
