15778f99f87718443b4d9deb144d82d505773dbb
andywar65 <andy.war1965@gmail.com>
2022-06-01 20:40:45 +0200

    save element tags from all ancestors

--- signals.py
+++ signals.py
@@ -8,6 +8,18 @@ from .models import Element, ElementTagValue
 def create_element_tags(sender, instance, created, **kwargs):
     if created:
         parent = instance.family
+        # get parent ancestors and generate element tag values
+        ancestors = parent.get_ancestors()
+        for ancestor in ancestors:
+            fam_values = ancestor.family_value.all()
+            for fam_value in fam_values:
+                elm_value = ElementTagValue(
+                    tag_id=fam_value.tag.id,
+                    element_id=instance.id,
+                    value=fam_value.value,
+                )
+                elm_value.save()
+        # generate element tag values from parent
         fam_values = parent.family_value.all()
         for fam_value in fam_values:
             elm_value = ElementTagValue(
