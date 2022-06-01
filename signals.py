16fd7ba83793ec21d3137f4defd2ab75dff91ff2
andywar65 <andy.war1965@gmail.com>
2022-06-01 20:19:28 +0200

    save element tags from parent

--- signals.py
+++ signals.py
@@ -1,10 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Element
+from .models import Element, ElementTagValue
 
 
 @receiver(post_save, sender=Element)
 def create_element_tags(sender, instance, created, **kwargs):
     if created:
-        pass
+        parent = instance.family
+        fam_values = parent.family_value.all()
+        for fam_value in fam_values:
+            elm_value = ElementTagValue(
+                tag_id=fam_value.tag.id, element_id=instance.id, value=fam_value.value
+            )
+            elm_value.save()
