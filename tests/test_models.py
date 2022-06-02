969b4c0545fad2d94a173d756d08f309e7553124
andywar65 <andy.war1965@gmail.com>
2022-06-02 15:18:12 +0200

    tested element image model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
 
-from djeotree.models import Element, Family, Tag, TagValue
+from djeotree.models import Element, ElementImage, Family, Tag, TagValue
 
 User = get_user_model()
 
@@ -61,3 +65,43 @@ class DjeotreeModelTest(TestCase):
         values = e.element_value.all()
         self.assertEquals(values.count(), 1)
         print("\n-Tested Element tag creation")
+
+
+@override_settings(MEDIA_ROOT=Path(settings.MEDIA_ROOT).joinpath("temp"))
+class ElementImageModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("\nTest element image model")
+        # Set up non-modified objects used by all test methods
+        u = User.objects.create(
+            username="raw.ydna56", password="P4s5W0r6", email="ydna@raw.com"
+        )
+        f = Family.objects.create(
+            user_id=u.uuid,
+            title="Family title",
+            path="0002",
+            depth=1,
+            numchild=0,
+        )
+        point = '{"type": "Point","coordinates": [12.493652,41.866288]}'
+        e = Element.objects.create(family_id=f.id, intro="bar", location=point)
+        img = ElementImage(element_id=e.id, description="taz")
+        img_path = Path(settings.STATIC_ROOT).joinpath("tests/image.jpg")
+        with open(img_path, "rb") as file:
+            content = file.read()
+        img.image = SimpleUploadedFile("image.jpg", content, "image/jpg")
+        img.save()
+
+    def tearDown(self):
+        """Checks existing files, then removes them"""
+        path = Path(settings.MEDIA_ROOT).joinpath("uploads/images/element/")
+        list = [e for e in path.iterdir() if e.is_file()]
+        for file in list:
+            Path(file).unlink()
+
+    def test_element_fb_image(self):
+        img = ElementImage.objects.get(description="taz")
+        self.assertEquals(img.image, "")
+        print("\n-Tested Element Image image")
+        self.assertEquals(img.fb_image.path, "uploads/images/element/image.jpg")
+        print("\n-Tested Element Image fb_image")
