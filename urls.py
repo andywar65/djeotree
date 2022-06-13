390235a10ba862a210125948dcc3ac8edf2420ed
andywar65 <andy.war1965@gmail.com>
2022-06-13 22:56:27 +0200

    author list view

--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
-from .views import ElementRedirectView  # noqa
-from .views import (
+from .views import (  # noqa
+    AuthorListView,
     BaseListView,
     ElementAuthorListView,
     ElementDetailView,
     ElementFamilyListView,
     ElementListView,
+    ElementRedirectView,
     FamilyListView,
 )
 
@@ -15,7 +16,7 @@ app_name = "geotree"
 urlpatterns = [
     path("", BaseListView.as_view(), name="base_list"),
     path(_("families"), FamilyListView.as_view(), name="family_list"),
-    path(_("authors"), ElementListView.as_view(), name="author_list"),
+    path(_("authors"), AuthorListView.as_view(), name="author_list"),
     path(_("author/<username>"), ElementAuthorListView.as_view(), name="author_detail"),
     path(
         _("authors/<username>/family/<pk>"),
--- views.py
+++ views.py
@@ -74,6 +74,26 @@ class FamilyListView(HxPageTemplateMixin, ListView):
         return context
 
 
+class AuthorListView(HxPageTemplateMixin, ListView):
+    model = Element
+    context_object_name = "elements"
+    template_name = "djeotree/htmx/author_list.html"
+
+    def get_queryset(self):
+        qs = Element.objects.filter(private=False)
+        if self.request.user.is_authenticated:
+            qs2 = Element.objects.filter(user_id=self.request.user.uuid, private=True)
+            qs = qs | qs2
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        authors = Element.objects.values_list("user__username", flat=True)
+        context["authors"] = list(dict.fromkeys(authors))
+        context["mapbox_token"] = settings.MAPBOX_TOKEN
+        return context
+
+
 class ElementListView(HxPageTemplateMixin, ListView):
     model = Element
     context_object_name = "elements"
