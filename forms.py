b6f6e2f65c4b34e90efaf6065c65321780484c58
andywar65 <andy.war1965@gmail.com>
2022-06-21 16:33:03 +0200

    disable user field

--- forms.py
+++ forms.py
@@ -1,11 +1,19 @@
-from django.forms import ModelForm
+from django.contrib.auth import get_user_model
+from django.forms import ModelChoiceField, ModelForm
+from django.utils.translation import gettext_lazy as _
 from leaflet.forms.widgets import LeafletWidget
 
 from .models import Element
 
+User = get_user_model()
+
 
 class ElementCreateForm(ModelForm):
+    user = ModelChoiceField(
+        label=_("Author"), queryset=User.objects.all(), disabled=True
+    )
+
     class Meta:
         model = Element
-        fields = ["user", "family", "intro", "geom"]
+        fields = ["user", "family", "intro", "geom", "private"]
         widgets = {"geom": LeafletWidget()}
