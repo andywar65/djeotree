07e2eb3412494cb42d07d8da9cf66bc14e16b688
andywar65 <andy.war1965@gmail.com>
2022-07-04 14:50:46 +0200

    create group on migration

--- apps.py
+++ apps.py
@@ -1,4 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.utils.translation import gettext as _
+
+
+def create_djeotree_group(sender, **kwargs):
+    from django.contrib.auth.models import Group, Permission
+    from django.contrib.contenttypes.models import ContentType
+
+    grp, created = Group.objects.get_or_create(name=_("GeoTree Manager"))
+    if created:
+        types = ContentType.objects.filter(app_label="djeotree").values_list(
+            "id", flat=True
+        )
+        permissions = Permission.objects.filter(content_type_id__in=types)
+        grp.permissions.set(permissions)
 
 
 class DjeotreeConfig(AppConfig):
@@ -7,3 +22,5 @@ class DjeotreeConfig(AppConfig):
 
     def ready(self):
         import djeotree.signals  # noqa
+
+        post_migrate.connect(create_djeotree_group, sender=self)
